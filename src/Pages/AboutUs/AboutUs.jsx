36046f9e71b3268e60c0d41d32d1420354a0395e
Shahadat-BD <shahadatstudent34@gmail.com>
2023-10-25 21:28:48 +0000

    about us section complete without responsive

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-
+import React from "react";
+import about_1 from "../../assets/images/about_us/person.jpg"
+import about_2 from "../../assets/images/about_us/parts.jpg"
 const AboutUs = () => {
-    return (
-        <div>
-            <h1>About us section </h1>
+  return (
+    <div className="my-20 ">
+      <div className="flex lg:flex-row flex-col justify-around gap-5">
+        <div className="relative">
+        <img className="w-[400px] h-[350px] rounded-lg" src={about_1} alt="" srcset="" />
+        <img className="w-[240px] h-[212px]  border-8 border-white rounded-lg absolute top-1/2 left-1/2" src={about_2} alt="" srcset="" />
         </div>
-    );
+        <div className="w-1/2">
+          <h3 className="text-[#FF3811] font-semibold text-md">About Us</h3>
+          <h5 className="text-4xl lg:w-1/2 font-bold my-3">We are qualified & of experience in this field</h5>
+          <p className="text-[#737373] font-normal lg:w-4/5">
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.{" "}
+          </p>
+          <p className="text-[#737373] font-normal lg:w-4/5 my-3">
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable.{" "}
+          </p>
+          <button className="bg-[#FF3811] text-white px-4 py-2 rounded-md font-semibold">Get More Info</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutUs;
